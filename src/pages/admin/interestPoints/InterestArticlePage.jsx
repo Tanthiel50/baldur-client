3fdd23ce3fa2c7766d3e5ee2254b8f5efb43f205
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2024-02-16 14:54:17 +0100

    interestarticlepage composant

--- src/pages/admin/interestPoints/InterestArticlePage.jsx
+++ src/pages/admin/interestPoints/InterestArticlePage.jsx
@@ -1,59 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Button, Table } from 'react-bootstrap';
-import Sidebar from '../../components/admin/Sidebard';
+import React, { useState, useEffect } from "react";
 import categoriesInfo from '../../components/categoriesInfo';
-import { useNavigate } from 'react-router-dom';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
-import { toast } from 'react-toastify';
 import GenericTablePage from '../../components/admin/GenericTablePage';
 
 const InterestArticlesPage = () => {
-  const [articles, setArticles] = useState([]);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchArticles = async () => {
-      try {
-        const response = await axios.get('http://127.0.0.1:8000/api/interestpoints');
-        setArticles(response.data);
-      } catch (error) {
-        console.error('Erreur lors de la récupération des interest points:', error);
-      }
-    };
-
-    fetchArticles();
-  }, []);
-
-  const handleCreateNewClick = () => {
-    navigate('/admin/create-interest-point'); 
-  };
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get('http://127.0.0.1:8000/api/interestpoints');
-      setArticles(response.data);
-    } catch (error) {
-      console.error('Erreur lors de la récupération des interest points:', error);
-    }
-  };
-
-  const handleDelete = async (articleId) => {
-    try {
-      await axios.delete(`http://127.0.0.1:8000/api/interestpoints/${articleId}`);
-      toast.success('Article supprimé avec succès');
-      fetchArticles();
-    } catch (error) {
-      toast.error('Erreur lors de la suppression de l\'article');
-      console.error('Erreur lors de la suppression:', error);
-    }
-  };
-
-  const handleEditClick = (articleId) => {
-    navigate(`/admin/edit-interest-point/${articleId}`);
-  };
-
-
   return (
     <GenericTablePage
       title="Liste des articles"
